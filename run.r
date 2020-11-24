3ac3e204c1a9258a7aec8b6c32d9e6ef2571a732
tdeswaef <65108283+tdeswaef@users.noreply.github.com>
2020-11-24 09:25:43 +0100

    Update run.r

--- run.r
+++ run.r
@@ -1,10 +1,17 @@
+### Install and load required libraries using pacman
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(shiny, tidyverse, readr, stringr, XLConnect)
-# library(shiny)
-# library(tidyverse)
-# library(readr)
+pacman::p_load(shiny, tidyverse, readr, stringr, XLConnect, rstudioapi)
 
-folder_address = 'C://Users//tdeswaef//Documents//Applications//UAVDataApp'
+### Set the PATH to the folder in which UAV data are stored (Q-drive)
+PATH <- "//clo.be/dfs/Data/iSense/Vluchtfotos/"
+
+### Set the folder address where the app files are located on your desktop
+folder_address = getActiveDocumentContext()$path %>% 
+                 dirname() %>% 
+                 str_replace_all("/", "//")
+
+### DON'T CHANGE
+### Set the working directory, read the metadata base files (.csv)
 setwd(dir = folder_address)
 Piloten <- read_csv("./InputFiles/Piloot.csv", col_names = F)[[1]] %>% as.vector()
 Velden <- read_csv("./InputFiles/Veld.csv", col_names = F)[[1]] %>% as.vector()
@@ -12,7 +19,6 @@ Projecten <- read_csv("./InputFiles/Project.csv", col_names = F)[[1]] %>% as.vec
 Sensoren <- read_csv("./InputFiles/Sensor.csv", col_names = F)[[1]] %>% as.vector()
 Gewassen <- read_csv("./InputFiles/Gewas.csv", col_names = F)[[1]] %>% as.vector()
 Spotters <- read_csv("./InputFiles/Spotter.csv", col_names = F)[[1]] %>% as.vector()
-#PATH <- "//clo.be/dfs/Data/iSense/Vluchtfotos/"
-PATH <- "C:/users/tdeswaef/Documents/00_DATA/"
 
+### Run the application in your standard browser
 runApp(folder_address, launch.browser=TRUE)
